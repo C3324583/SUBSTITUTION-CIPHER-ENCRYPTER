15910a115543cfd99e1e5ce8366afcea28940ddb
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-04-16 09:48:21 +1000

    Update subencrypter.c

--- subencrypter.c
+++ subencrypter.c
@@ -3,24 +3,47 @@
 
 
 int main(){
-    //initialising variables
-	char text[1000], substitution[100], element;
-	int n, i, rotationkey;
+    //initialisation of variables and arrays
+	char text[1000], substitution[100];
+	const char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n, i;
 	
-	printf("Enter message: ");//prompt the user to imput a message
-	scanf(" %[^\n]s", text);//store the text imputted by the user in the array "text"
-	printf("Enter an alphabet substitution by typing 26 consecutive letters, where the first will replace A, the second will replace B and so on");//prompt the user to imput an alphabet substitution
-	scanf(" %[^\n]s", substitution);//store the rotation value in the variable "rotationkey"
+	printf("Enter message: ");//prompts the user to imput a message
+	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
+	printf("Enter an alphabet substitution by typing 26 consecutive letters, where the first will replace A, the second will replace B and so on: ");//prompts the user to imput an alphabet substitution
+	scanf("%s", substitution);//stores the alphabet substitution in the array "substitution"
     
-    //turn lowercase into uppercase
-    if(substitution[i] >= 'a' && substitution[i] <= 'z'){
-		substitution[i] = substitution[i] - 32;
-	}
-
-			
-    for(i=0; i<26; i++){
-        substitution[i] = substitution[i] - i;
-        text[i] = 
+    /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
+    "NULL" character*/
+    for(n=0; text[n]!='\0'; n++){
+  
+	    //turns lowercase letters in the array "text" into UPPERCASE by subtracting 32
+     	if(text[n] >= 'a' && text[n] <= 'z'){
+		    text[n] = text[n] - 32;
+     	}
+     
+        //prints the character at index "n" to the screen if it is not a letter
+        if(text[n] < 'A' || text[n] > 'Z'){
+                printf("%c", text[n]);
+        }
+        
+        /* This loop tests if the character at index "n" is equal to any of the letters in the array "ALPHABET" 
+        (index 0 to index 25), and if true, the character is substituted with the corresponding chracter in the 
+        array "substitution" */
+        for(i=0; i<26; i++){
+            
+            /* This "if" statement turns lowercase letters in the array "substitution" into UPPERCASE by subtracting
+            32 before the substitution is done */
+            if(substitution[i] >= 'a' && substitution[i] <= 'z'){
+		        substitution[i] = substitution[i] - 32;
+	        }
+             
+            /* This "if" statement tests if the charater in "text" at index "n" is equal to any character in the
+            "ALPHABET". If true, the character at index "i" of "substitution" is printed to the screen*/
+            if (text[n] == ALPHABET[i]){
+                printf("%c", substitution[i]);
+            }
+        }
     }
     return 0;
 }
